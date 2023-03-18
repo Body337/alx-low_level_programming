c41dfc841a2338ac94ff52b3f41858dc59439673
Body337 <xxxlarge6665@gmail.com>
2023-03-18 07:59:01 -0700

    new

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,8 @@
-/*
- * File--> 4-puts.c
- * 
- */
-
 #include<stdio.h>
-
-/*
+/**
  * Description: main - Prints out a string to stdout.
  * Return: 0 if success.
  */
-
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
