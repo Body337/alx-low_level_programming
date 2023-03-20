471a3097820252f1540f3a43d35c5c9174c89c7e
Body337 <xxxlarge6665@gmail.com>
2023-03-20 11:48:29 -0700

    new

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - main block
+ * Descreption: prints all base 16 number
+ * Return: 0
+ */
+int main(void)
+{
+	char a = 'a';
+	char z = 'f';
+	int x = 0;
+	
+	for (; x < 10; x++)
+	{
+		putchar(x + '0');
+	}
+	for (; a <= z; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
