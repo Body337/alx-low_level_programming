f1c9fd4888db1220018414c8e5ceb4aa58858afc
Body337 <xxxlarge6665@gmail.com>
2023-03-20 11:59:46 -0700

    new

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - main block
+ * Descreption: prints possible combnination
+ * Return: 0
+ */
+int main(void)
+{
+	int x = 0;
+
+	while (x<=9)
+	{
+		putchar(x + '0');
+		if (x<9)
+		{
+			putchat(',');
+			putchar(' ');
+		}
+		x++;
+	}
+	putchar('\n');
+	return (0);
+}
