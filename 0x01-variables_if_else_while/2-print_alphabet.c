4c2b1e659e5a9c95df87b035005c727b6be31c52
Body337 <xxxlarge6665@gmail.com>
2023-03-20 06:09:17 -0700

    new

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - main block
+ * Description: takes an upper case letter
+ * and display it in lower case
+ * Return: 0
+ */
+int main(void)
+{
+	char alpha_bet = 'a';
+	char last_bet = 'z';
+	for (;alpha_bet <= last_bet ;alpha_bet++)
+	{
+		putchar(alpha_bet);
+	}
+	return (0);
+}
