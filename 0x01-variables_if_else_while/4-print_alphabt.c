0109012b0e4df324aef663c3a6c317630c13a58d
Body337 <xxxlarge6665@gmail.com>
2023-03-20 11:02:42 -0700

    new

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - main block
+ * Descreption; prints all alphabet except q e
+ * Return: 0
+ */
+int main(void)
+{
+	char frits = 'a';
+	char last = 'z';
+	
+	for (; frist <= last; frist++)
+	{
+		if (frist == 'q' || frist == 'e')
+		{
+		
+		}
+		else
+			putchar(frist);
+	}
+	return (0);
+
+}
