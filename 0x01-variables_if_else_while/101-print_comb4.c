ec1820ea2c08d9dab7e4b79d3a0e65d774f8142c
Body337 <xxxlarge6665@gmail.com>
2023-03-21 13:11:11 -0700

    new

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main - main block
+ * Descreption: prints the combinations of three digits
+ * Return: 0
+ */
+int main(void)
+{
+	int a, b, c, d;
+
+	a = 10;
+
+	while (a < 100)
+	{
+		b = a % 10;
+		c = (a / 10) % 10;
+		d = a / 100;
+
+		if (d < b && d < c && c < b)
+		{
+			putchar(d + '0');
+			putchar(c + '0');
+			putchar(b + '0');
+
+			if (a < 789)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+		a++
+	}	
+	putchar('\n');
+	return (0);
+}
