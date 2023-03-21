7bb3441331d2a3edc3e0e8ed09e37def500771af
Body337 <xxxlarge6665@gmail.com>
2023-03-21 09:42:06 -0700

    new

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - main block
+ * Descreption: prints the different combination of two digits
+ * Return: 0
+ */
+int main(void)
+{
+	int a, b, c;
+
+	a = 0;
+
+	while (a < 100)
+	{
+		b = a%10;
+		c = a/10;
+
+		if (c < b)
+		{
+			putchar(b + '0');
+			putchar(c + '0');
+
+			if (a < 89)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n')
+	return (0);
+}
