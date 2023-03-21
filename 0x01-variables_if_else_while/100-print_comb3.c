8e80ff77dd9cadcbdf0eee05ffded906d76fa0c9
Body337 <xxxlarge6665@gmail.com>
2023-03-21 09:45:44 -0700

    new

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -13,8 +13,8 @@ int main(void)
 
 	while (a < 100)
 	{
-		b = a%10;
-		c = a/10;
+		b = a % 10;
+		c = a / 10;
 
 		if (c < b)
 		{
@@ -27,6 +27,7 @@ int main(void)
 				putchar(' ');
 			}
 		}
+		a++
 	}
 	putchar('\n');
 	return (0);
