a8895f88f57350bbdbf6c584a6d4d1ab49b6d94d
Body337 <xxxlarge6665@gmail.com>
2023-03-22 13:49:56 -0700

    new repo

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <main.h>
+
+/**
+ * main - main block
+ * prints _putcahr
+ * Return: 0;
+ */
+int main(void)
+{
+	char c[] = "_putchar";
+	int i = 0;
+
+
+	for (; i < c[]; i++)
+	{
+		_putchar(c[i]);
+	}
+	_putchar('\n');
+	return (0);
+}
