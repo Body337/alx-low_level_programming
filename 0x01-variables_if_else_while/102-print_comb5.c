da284897b632876642aea5683d1355f7fb98c547
Body337 <xxxlarge6665@gmail.com>
2023-05-20 14:26:24 +0300

    comb5

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,16 +1,17 @@
 #include<stdio.h>
 /*
  * main - main block
- * Descreption : prints all numbers from 1 to 99 in 2 combination
+ * Descreption: prints all numbers from 1 to 99 in 2 combination
  * Return: 0
  */
-int main()
+int main(void)
 {
 	int x, y, z, q, r, s;
+
 	x = 0;
 	y = 0;
 
-	for(x = 0; x <= 98; x++)
+	for (x = 0; x <= 98; x++)
 	{
 		for (y = 0; y <= 99; y++)
 		{
@@ -28,14 +29,13 @@ int main()
 				putchar(s + '0');
 				if (x == 98 && y == 99)
 				{
-					
 				}
 				else
 				{
 					putchar(',');
 					putchar(' ');
 				}
-			}	
+			}
 		}
 	}
 	putchar('\n');
