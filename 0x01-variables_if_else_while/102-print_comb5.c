a45e11d467685287fbe9a3ae63e9300f5331cdf6
Body337 <xxxlarge6665@gmail.com>
2023-05-20 14:18:56 +0300

    comb5

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,26 +7,26 @@
 int main()
 {
 	int x, y, z, q, r, s;
-	x = 98;
-	y = 99;
+	x = 0;
+	y = 0;
 
-	for(int i = 0; i <= x; i++)
+	for(x = 0; x <= 98; x++)
 	{
-		for (int w = 0; w <= y; w++)
+		for (y = 0; y <= 99; y++)
 		{
-			z = i / 10;
-			q = i % 10;
-			r = w / 10;
-			s = w % 10;
+			z = x / 10;
+			q = x % 10;
+			r = y / 10;
+			s = y % 10;
 
-			if (i < w)
+			if (x < y)
 			{
 				putchar(z + '0');
 				putchar(q + '0');
 				putchar(' ');
 				putchar(r + '0');
 				putchar(s + '0');
-				if (i == 98 && w == 99)
+				if (x == 98 && y == 99)
 				{
 					
 				}
