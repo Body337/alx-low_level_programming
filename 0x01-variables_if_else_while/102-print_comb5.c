6463f8dcf931052c37d39985b1aac127f71ce807
Body337 <xxxlarge6665@gmail.com>
2023-05-20 14:13:14 +0300

    comb5

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,41 @@
+#!/bin/bash
+/*
+ * main - main block
+ * Descreption : prints all numbers from 1 to 99 in 2 combination
+ * Return: 0
+ */
+int main()
+{
+	int x, y, z, q, r, s;
+	x = 98;
+	y = 99;
+
+	for(int i = 0; i <= x; i++)
+	{
+		for (int w = 0; w <= y; w++)
+		{
+			z = i / 10;
+			q = i % 10;
+			r = w / 10;
+			s = w % 10;
+
+			if (i < w)
+			{
+				putchar(z + '0');
+				putchar(q + '0');
+				putchar(' ');
+				putchar(r + '0');
+				putchar(s + '0');
+				if (i == 98 && w == 99)
+				{
+					
+				}
+				else
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}	
+		}
+	}
+}
