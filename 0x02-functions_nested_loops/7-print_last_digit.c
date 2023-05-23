3e049eab039c52a4009cc3989975ad5a1d338fdc
Body337 <xxxlarge6665@gmail.com>
2023-05-23 20:54:14 +0000

    new

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_last_digit - prints the last digit
+ * @n: to be tested
+ * Return: the value of the lastt digit
+ */
+int print_last_digit(int n)
+{
+	return(n%10);
+}
