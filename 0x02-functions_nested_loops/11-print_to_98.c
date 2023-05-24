0af8ad91c5105d816b6529860fda8e9a22cc9dd4
Body337 <xxxlarge6665@gmail.com>
2023-05-24 22:05:07 +0000

    maybe 98 is something

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,91 +1,43 @@
+#include "holberton.h"
 #include <stdio.h>
-#include "main.h"
 /**
- * print_to_98 - always prints to 98
- * @n: a number to be given
- */
+  * print_to_98 - Prints all natural numbers from n to 98
+  * @n: The number to start printing from
+  *
+  * Return: Always 0.
+  */
 void print_to_98(int n)
 {
-	int hunds, tens, digit;
-
-	if (n < 0)
+	if (n <= 98)
 	{
-		hunds = (n * -1) / 100;
-		digit = (n * -1) % 10;
-		tens = ((n * -1) / 10) % 10;
-	}
-	else if (n > 0)
-	{
-		hunds = n / 100;
-		digit = n % 10;
-		tens = (n / 10) % 10;
-	}
-
-	if (n < 0)
-	{
-		for (; n < 98; n++)
+		for (; n <= 98; n++)
 		{
-			if (n <= -100)
+			if (n == 98)
 			{
-				_putchar('-');
-				_putchar(hunds + '0');
-				_putchar(tens + '0');
-				_putchar(digit + '0');
-				_putchar(',');
-				_putchar(' ');
+				printf("%d", n);
+				printf("\n");
+				break;
 			}
-			else if (n > -100)
+			else
 			{
-				_putchar('-');
-				_putchar(tens + '0');
-				_putchar(digit + '0');
-				_putchar(',');
-				_putchar(' ');
+				printf("%d, ", n);
 			}
 		}
 	}
-	if (n >=0 && n < 98)
+	else
 	{
-		for (; n < 98; n++)
+		for (; n >= 98; n--)
 		{
-			if (n < 100 && n > 9)
+			if (n == 98)
 			{
-				_putchar(tens + '0');
-				_putchar(digit + '0');
-				_putchar(',');
-				_putchar(' ');
+				printf("%d", n);
+				printf("\n");
+				break;
 			}
-			else if (n < 10 )
+			else
 			{
-				_putchar(digit + '0');
-				_putchar(',');
-				_putchar(' ');
+				printf("%d, ", n);
 			}
 		}
 	}
-	else if (n > 98)
-	{
-		
-		for (; n > 98; n--)
-		{
-			if (n >= 100)
-			{
-				_putchar(hunds + '0');
-				_putchar(tens + '0');
-				_putchar(digit + '0');
-				_putchar(',');
-				_putchar(' ');
-			}
-			else if (n < 100)
-			{
-				_putchar(tens + '0');
-				_putchar(digit + '0');
-				_putchar(',');
-				_putchar(' ');
-			}
-		}
-	}
-	_putchar(9 + '0');
-	_putchar(8 + '0');
-	_putchar('\n');
 }
