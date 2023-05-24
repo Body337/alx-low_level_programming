f3e7837d1950d8f691a8b7abaeb2c3e7f039c3e1
Body337 <xxxlarge6665@gmail.com>
2023-05-24 12:44:01 +0000

    everyone must know the table right?

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,12 +9,12 @@ void times_table(void)
 {
 	int row, column, num;
 
-	for (row = 0; row >= 9; row++)
+	for (row = 0; row <= 9; row++)
 	{
-		for (column = 0; column >= 9; column++)
+		for (column = 0; column <= 9; column++)
 		{
 			num = row * column;
-			
+
 			if (num == 0)
 			{
 				_putchar(num + '0');
