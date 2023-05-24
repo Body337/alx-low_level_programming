3ca2656ba295e177ff5ff6953d236ca747118da3
Body337 <xxxlarge6665@gmail.com>
2023-05-24 12:46:02 +0000

    everyone must know the table right?

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -11,17 +11,14 @@ void times_table(void)
 
 	for (row = 0; row <= 9; row++)
 	{
+		_putchar(0 + '0');
+		_putchar(',');
+
 		for (column = 0; column <= 9; column++)
 		{
 			num = row * column;
 
-			if (num == 0)
-			{
-				_putchar(num + '0');
-				_putchar(',');
-				continue;
-			}
-			else if (num < 10 && num != 0)
+			if (num < 10 && num != 0)
 			{
 				_putchar(' ');
 				_putchar(' ');
