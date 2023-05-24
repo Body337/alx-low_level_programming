f1137d4676d484496953e46708352710e0f5794f
Body337 <xxxlarge6665@gmail.com>
2023-05-24 15:59:46 +0000

    maybe 98 is something

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,24 +8,24 @@ void print_to_98(int n)
 {
 	if (n > 98)
 	{
-		_putchar(n);
+		_putchar(n + '0');
 		for (; n < 98; n++)
 		{
-			_putchar(n);
+			_putchar(n + '0');
 			_putchar(',');
 			_putchar(' ');
 		}
 	}
-	else if(n < 98)
+	else if (n < 98)
 	{
-		_putchar(n);
+		_putchar(n + '0');
 		for (; n > 98; n++)
 		{
-			_putchar(n);
+			_putchar(n + '0');
 			_putchar(',');
 			_putchar(' ');
 		}
 	}
-	_putchar(n);
+	_putchar(n + '0');
 	_putchar('\n');
 }
