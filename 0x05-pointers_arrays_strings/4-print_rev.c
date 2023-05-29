b9543558ba7205404f45901964a4eaa7b153fd80
Body337 <xxxlarge6665@gmail.com>
2023-05-29 20:02:01 +0000

    i like it in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include <stio.h>
+#include "main.h"
+/**
+ * print_rev - prints the string in reverse order
+ * @s: a pointer to the string str
+ *
+ * Return: 0 on success
+ */
+void print_rev(char *s)
+{
+	int i;
+
+	i = _strlen(s);
+
+	for (;i >= 0; i--)
+	{
+		*s = i - 1;
+		_putchar(*s)
+	}
+}
