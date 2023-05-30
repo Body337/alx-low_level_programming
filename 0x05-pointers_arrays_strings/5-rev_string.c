6ac5c15319cb41ff7faf8f933e5281c73830835a
Body337 <xxxlarge6665@gmail.com>
2023-05-30 20:22:05 +0000

    i almost raged

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * rev_string - reverse a string
+ * @s: a string to be reversed
+ *
+ * Return: 0 on success
+ */
+void rev_string(char *s)
+{
+	int x, c;
+	char z, w;
+
+	for (x = 0; s[x] != '\0';)
+	{
+		x++;
+	}
+
+	c = 0;
+
+	while (x != c)
+	{
+		z = s[x];
+		w = s[c];
+		s[x] = w;
+		s[c] = z;
+		c++;
+		x--;
+	}
+}
