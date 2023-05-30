7aaf3bdc381904951cad9859cd27058a6a9d8093
Body337 <xxxlarge6665@gmail.com>
2023-05-30 21:37:07 +0000

    i really dont know why

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -10,21 +10,21 @@ void puts_half(char *str)
 {
 	int x, half;
 
-	for(x = 0; str[x] != '\0';)
+	for (x = 0; str[x] != '\0';)
 	{
 		x++;
 	}
 
-	if ((x +1) % 2 == 0)
+	if ((x + 1) % 2 == 0)
 	{
-		half = x / 2;
+		half = (x - 1) / 2;
 	}
 	else
 	{
 		half = (x - 1) / 2;
 	}
-	
-	for (; half <= x; half++)
+
+	for (; str[half] != '\0'; half++)
 	{
 		_putchar(str[half]);
 	}
