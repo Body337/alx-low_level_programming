52bc5865d09454e6bc220eb568302497d096a4f1
Body337 <xxxlarge6665@gmail.com>
2023-05-30 20:46:01 +0000

    idk

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * puts2 - put the other char every time
+ * @str: a string to be printed
+ *
+ * Return: 0 on success
+ */
+void puts2(char *str)
+{
+	int x, c;
+
+	x = 0;
+	c = 0;
+
+	while (str[x] != '\0')
+	{
+		x++;
+	}
+
+	while (c < x)
+	{
+		_putchar(str[c]);
+		c += 2;
+	}
+}
