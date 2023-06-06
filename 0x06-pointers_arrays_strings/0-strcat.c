b009e27e57a8c74dae4267ebdad2c86eb6b7501a
Body337 <xxxlarge6665@gmail.com>
2023-06-06 12:28:25 +0000

    adds to a string

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _strcat - works the same as strcat
+ * @dest: a pointer to a string
+ * @src: a pointer to a string
+ * Return: dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	int x, y, z, i;
+
+	i = 0;
+	z = 0;
+	
+	for (x = 0; *src != '\0';src++)
+	{
+		z++;
+		x++;
+	}
+	for (y = 0; *dest != '\0'; dest++)
+	{
+		y++;
+	}
+
+	x = y + x;
+	y++;
+
+	while (y < x && i < z)
+	{
+		dest[y] = src[i];
+		y++;
+		i++;
+	}
+}
