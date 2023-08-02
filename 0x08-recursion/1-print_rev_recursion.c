ddf594c85b8f23bc60f4a1bd0d2e1864c172ff87
Body337 <xxxlarge6665@gmail.com>
2023-08-02 11:50:24 +0000

    puts recursion in rev

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * _print_rev_recursion - work as puts recursion but in reverse
- * @s - a pointer to a char array
+ * @s: a pointer to a char array
  * Return: nothing;
  */
 void _print_rev_recursion(char *s)
@@ -12,11 +12,7 @@ void _print_rev_recursion(char *s)
 	{
 		_print_rev_recursion(s);
 		_putchar(*s);
-		if (*(s - 1) == '\0')
-		{
-			_putchar('\n');
-		}
-		return;
 	}
-	
+	_putchar('\n');
+
 }
