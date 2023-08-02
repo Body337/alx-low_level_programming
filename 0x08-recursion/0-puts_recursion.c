4374971fb3a567b13637473657858e16034c36eb
Body337 <xxxlarge6665@gmail.com>
2023-08-02 11:09:46 +0000

    puts with recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _puts_recursion - puts with recursion
+ * @s: a pointer to a char array
+ * Return: nothing
+ */
+void _puts_recurstion(char *s)
+{
+	_putchar(*s);
+	*s--;
+	if (*s == '/0')
+	{
+		exit;
+	}
+	else
+		_puts_recursion(*s);
+}
