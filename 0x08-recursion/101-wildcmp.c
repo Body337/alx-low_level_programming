00d0a5bb6b880204e14f8b2aec24200722dde02e
Body337 <xxxlarge6665@gmail.com>
2023-08-03 00:21:45 +0000

    i hope i understood

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * wildcmp - compares tow strings
+ * @s1: string to be compared with
+ * @s2: string to be compared
+ *
+ * Return: 1 if they smiller 0 if not
+ */
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == '\0' && *s2 == '\0')
+	{
+		return (1);
+	}
+	if (*s1 == *s2)
+	{
+		return (wildcmp(s1 + 1,s2 + 1))
+	}
+	if (*s2 == '*')
+	{
+		if (*(s2 + 1) == '*')
+		{
+			return (wildcmp(s1 , s2 + 1))
+		}
+		if (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1))
+		{
+			return (1);
+		}
+	}
+	return (0);
+
+}
