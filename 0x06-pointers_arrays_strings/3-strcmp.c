635cf3e2df3ed69ac213bc01a7fa8fd1f03f40ac
Body337 <xxxlarge6665@gmail.com>
2023-08-04 22:32:32 +0000

    strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -12,9 +12,9 @@ int _strcmp(char *s1, char *s2)
 	int i;
 	int z = 0;
 
-	for (i = 0; (*s1 != '\0' || *s2 != '0'); i++)
+	for (i = 0; *s1 != '\0' || *s2 != '\0'; i++)
 	{
-		if (*(s1 + i) != *(s2 + 1))
+		if (*(s1 + i) != *(s2 +i))
 		{
 			z = (*s1 - *s2)
 		}
