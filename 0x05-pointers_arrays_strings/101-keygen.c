dc43b61b7b0c38f8ada0ad36886ebc0d6d673f12
Body337 <xxxlarge6665@gmail.com>
2023-08-04 17:34:25 +0000

    crack it

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - main block
+ * Descreption: genrates a random number for  101-crackme
+ * Return: always return 0
+ */
+int main(void)
+{
+	srand(time(NULL));
+	printf(%d, rand());
+	return (0);
+}
