9ffcd2c752ffa66f7cd0aecdcd62a9c79aacf84a
Body337 <xxxlarge6665@gmail.com>
2023-08-05 00:23:11 +0000

    memcpy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _memcpy - copies n number from string src to string dest
+ * @dest: a string to be modified
+ * @src: string to be modified from
+ *
+ * Return: pointer to dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int x;
+
+	for (x = 0; x < n; x++)
+	{
+		dest[x] = src[x];
+	}
+	return (dest);
+}
