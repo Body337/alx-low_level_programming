a5be321349e46140e7351f4ffe3f9c35e9edf4a2
Body337 <xxxlarge6665@gmail.com>
2023-08-05 01:13:36 +0000

    strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -9,12 +9,17 @@
 char *_strchr(char *s, char c)
 {
 	int a;
+
 	for (a = 0; s[a]; a++)
 	{
 		if (s[a] == c)
 		{
 			return (s + a);
 		}
+	}
+	if (c == '\0')
+	{
+		return (s + a);
 	}
 	return ('\0');
 }
