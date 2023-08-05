0abe29b05c3d36f1acad2bc78b3cacd3d04c2c0a
Body337 <xxxlarge6665@gmail.com>
2023-08-05 02:13:44 +0000

    str

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * _strstr - finds the frist occurrence of a substring
+ * @haystack: the frist string
+ * @needle: the substring
+ *
+ * Return: the frist occurrence of the substring
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int a = 0;
+	int b = 0;
+
+	while (haystack[a])
+	{
+		while (needle[b])
+		{
+			if (needle[b] == haystack[a])
+			{
+				a++;
+				b++;
+			}
+		}
+		if (needle[b] == '\0')
+		{
+			return (haystack + a);
+		}
+		else
+		{
+			a = a - b;
+		}
+		a++;
+	}
+	return ('\0');
+}
