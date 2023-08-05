3d5769ad91e26298352002eed5d7b8a3d629455c
Body337 <xxxlarge6665@gmail.com>
2023-08-05 01:08:12 +0000

    strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,16 +1,15 @@
 #include "main.h"
-
 /**
  * strchr - search for char c in string s
  * @s: pointer to a string
  * @c: a char to be searched for
- * 
+ *
  * Return: pointer to s or null if c not found
  */
 char *_strchr(char *s, char c)
 {
 	int a;
-	for (a = 0; s[a] != '\0'; a++)
+	for (a = 0; s[a]; a++)
 	{
 		if (s[a] == c)
 		{
