85d4ea9213a33d5ef43caad0d6f5f173c23ba140
Body337 <xxxlarge6665@gmail.com>
2023-08-06 19:49:34 +0000

    times table

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,49 @@
+#include "main.h"
+/**
+ * print_time_table
+ * @n: the n time table
+ *
+ * Return: nothing
+ */
+void print_time_table(int n)
+{
+	int i, j;
+
+	if (n < 0 || n > 15)
+		return;
+	for (i = 0; i <= n; i++)
+	{
+		_putchar('0');
+		_putchar(',');
+		for (j = 1; j <= n; j++)
+		{
+			if (i * j <= 9)
+			{
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar('0' + (i * j));
+			}
+			else if (i * j >= 10 && i * j  <= 99)
+			{
+				_putchar(' ');
+				_putchar(' ');
+				_putchar('0' + ((i * j) / 10));
+				_putchar('0' + ((i * j) % 10));
+			}
+			else if (i * j >= 100 && i * j <= 999)
+			{
+				_putchar(' ');
+				_putchar('0' + ((i * j) / 100));
+				_putchar('0' + (((i * j) % 100) / 10));
+				_putchar('0' + ((i * j) % 10));
+			}
+			if (j != n)
+			{
+				_putchar(',');
+			}
+		}
+		_putchar('\n');
+	}
+	_putchar('\n');
+}
