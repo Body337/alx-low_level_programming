e7f68eb54703ca566969849490f6ec42653852c5
Body337 <xxxlarge6665@gmail.com>
2023-08-07 18:09:21 +0000

    strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -5,14 +5,14 @@
  * _strdup - return a pointer to a newly allocated memory
  * @str: a pointer to a string
  *
- * Return; a pointer to the newly allocated memory
+ * Return: a pointer to the newly allocated memory
  */
 char *_strdup(char *str)
 {
 	int x = 0, i = 0;
 	char *s;
 
-	if (str[0] == '\0')
+	if (str == NULL)
 		return (NULL);
 
 	while (str[i])
