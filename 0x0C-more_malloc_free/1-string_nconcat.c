3bba35dc0b9210af2ee7e3a9a7572871af5394ba
Body337 <xxxlarge6665@gmail.com>
2023-08-10 17:29:09 +0000

    new concat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -12,36 +12,29 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	unsigned int sl1 = 0, sl2 = 0, z = 0, x = 0;
 	char *arr;
-	
+
 	if (s2 == NULL)
-	{
 		s2 = "";
-	}
 	if (s1 == NULL)
-	{
 		s1 = "";
-	}
+
 	while (s1[sl1])
 		sl1++;
 	while (s2[sl2])
 		sl2++;
 
 	if (n >= sl2)
-		arr = (char *)malloc(sizeof(char) * (sl1 + sl2 + 1));
+		arr = malloc(sizeof(char) * (sl1 + sl2 + 1));
 	else
-		arr = (char *)malloc(sizeof(char) * (sl1 + n + 1));
-	
+		arr = malloc(sizeof(char) * (sl1 + n + 1));
 	if (arr == NULL)
-	{
-		free (arr);
 		return (NULL);
-	}
+
 	while (x < sl1)
 	{
 		arr[x] = s1[x];
 		x++;
 	}
-
 	while (s2[z] != '\0' && z < n)
 	{
 		arr[x] = s2[z];
