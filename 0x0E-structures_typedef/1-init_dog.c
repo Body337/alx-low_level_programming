2642f661c55b2483ae6dff685c44254a16188b36
Body337 <xxxlarge6665@gmail.com>
2023-08-14 18:36:28 +0000

    poppy 2

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+/**
+ * init_dog - intinaliez data to its variables
+ * @d: astruct type
+ * @name: a string
+ * @age: a float
+ * @owner: a string
+ *
+ * Return: nothing
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	(*d).name = name;
+	(*d).age = age;
+	(*d).owner = owner;
+}
