da7f9d5959b23fa69c088d21e95d70e17cb0583c
Body337 <xxxlarge6665@gmail.com>
2023-08-14 17:45:26 +0000

    poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef _HEADER_
+#define _HEADER_
+/**
+ * struct dog - date of a dog
+ * @name: a string member
+ * @age: a float member
+ * @owner: a string member
+ *
+ * Description: date of a dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
