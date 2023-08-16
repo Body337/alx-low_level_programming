52c6717a3634b88699d0e4ba848c0c727523ac34
Body337 <xxxlarge6665@gmail.com>
2023-08-16 19:56:03 +0000

    print the name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+/**
+ * print_name - prints the name
+ * @name: a string name
+ * @f: a function pointer
+ *
+ * Return: nothing
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	(*f)(name);
+}
