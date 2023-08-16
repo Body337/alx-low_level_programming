4dd707707d859d1e16b676cfba00ce45cb3bcb6f
Body337 <xxxlarge6665@gmail.com>
2023-08-16 20:06:35 +0000

    array action

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,20 @@
+#include "function_pointers.h"
+/**
+ * array_iterator - makes an action on the array
+ * @array: array of ints
+ * @size: size of the array
+ * @action: action to be taken on the array
+ *
+ * Return: nothing
+ */
+void array_iterator(int *array , size_t size, void (*action)(int))
+{
+	size_t x;
+	if (array != NULL && size != 0 && action != NULL)
+	{
+		for (x = 0; x < size; x++)
+		{
+			(*action)(array[x]);
+		}
+	}
+}
