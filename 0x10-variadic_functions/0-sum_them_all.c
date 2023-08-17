2d843eb712e2a6a505b77479ac691716448d33b5
Body337 <xxxlarge6665@gmail.com>
2023-08-17 18:50:23 +0000

    sum them up

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+/**
+ * sum_them_all - sum all given numbers
+ * @n: number of given numbers
+ *
+ * Return: sum of the number
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list ap;
+	unsigned int i, sum = 0;
+
+	va_start(ap, n);
+
+	for (i = 0; i < n; i++)
+		sum += va_arg(ap, int);
+
+	va_end(ap);
+	return (sum);
+}
