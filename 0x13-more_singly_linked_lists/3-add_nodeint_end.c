b6a327f7241f6115af9d9619e5da4b4c302d407b
Body337 <xxxlarge6665@gmail.com>
2023-08-29 22:11:33 +0000

    i hopt its fixed

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -12,28 +12,26 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 {
 	listint_t *new, *temp;
 
-	if (n != NULL)
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->n = n;
+	new->next = NULL;
+
+	if (*head == NULL)
 	{
-		new = malloc(sizeof(listint_t));
-		if (new == NULL)
-			return (NULL);
+		*head = new;
+		return (*head);
+	}
+	else
+	{
+		temp = *head;
+		while (temp->next)
+			temp = temp->next;
 
-		new->n = n;
-		new->next = NULL;
-
-		if (*head == NULL)
-		{
-			*head = new;
-		}
-		else
-		{
-			temp = *head;
-			while (temp->next)
-				temp = temp->next;
-
-			temp->next = new;
-			return (new);
-		}
+		temp->next = new;
+		return (temp);
 	}
 	return (0);
 }
