e3814501d94dc872b9e353d5d081638e965305d1
Body337 <xxxlarge6665@gmail.com>
2023-08-29 22:22:59 +0000

    freed?

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+#include <stdlib.h>
+/**
+ * free_listint2 - free list
+ * @head: pointer to the list
+ *
+ * Retunr: nothing
+ */
+void free_listint2(listint_t **head)
+{
+	list_t *list;
+
+	while (*head)
+	{
+		list = (*head);
+		(*head) = (*head)->next;
+		free(list->n);
+		free(list);
+	}
+	free (**head)
+	head = 0;
+}
