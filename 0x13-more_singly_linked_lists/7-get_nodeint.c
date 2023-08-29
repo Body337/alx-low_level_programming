acb4b59f6bacbf5b9c56113a0c87dfe74312c7da
Body337 <xxxlarge6665@gmail.com>
2023-08-29 23:40:18 +0000

    index list

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include "lists.h"
+#include <stdio.h>
+
+/**
+  * get_nodeint_at_index - return n at index
+  * @head: pointer to list
+  * @index: index of the list
+  *
+  * Return: null if not exist
+  */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int i = 0;
+	listint_t *first, *temp; 
+
+	first = head;
+
+	while (i < index && head)
+	{
+		head = head->next;
+		i++;
+	}
+	if (i != index)
+		return (NULL);
+
+	temp = head;
+	head = first;
+
+	return (temp);
+}
