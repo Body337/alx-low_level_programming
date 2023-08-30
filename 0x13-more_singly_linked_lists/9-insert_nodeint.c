fccdadb1c0d3887cc23db3b5d35fb85417afef84
Body337 <xxxlarge6665@gmail.com>
2023-08-30 00:21:24 +0000

    added at index?

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,39 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "lists.h"
+listint_t *add_nodeint(listint_t **head, const int n);
+/**
+  * insert_nodeint_at_index - add n at index idx
+  * @head: pointer to list
+  * @idx: index to be added to
+  * @n: number to be added
+  *
+  * Return: pointer to the added node
+  */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	unsigned int i = 0;
+	listint_t *node, *next;
+
+	if (head)
+	{
+		node = malloc(sizeof(listint_t));
+		if (node == NULL)
+			return (NULL);
+		
+		node->n = n;
+		next = *head;
+		while(next && i < idx)
+		{
+			next = next->next;
+			i++;
+		}
+		if(i != idx)
+			return (NULL);
+		node->next = next->next;
+		next->next = node;
+
+		return (node);
+	}
+	return (NULL);
+}
