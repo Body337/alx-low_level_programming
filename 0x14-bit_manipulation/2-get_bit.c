d35068387fcc6f53b96a32b38d88ae1b715fa347
Body337 <xxxlarge6665@gmail.com>
2023-09-01 23:47:48 +0000

    git bit

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+  * get_bit - Gets the value of a bit at a given index
+  * @n: The number to find the index
+  * @index: The index to find
+  *
+  * Return: the bit
+  */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	n = n >> index;
+
+	if (n)
+	{
+		return ((n & 1) ? 1 : 0);
+	}
+	return (-1);
+}
