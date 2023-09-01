948aae9a0164fc72c801ec436dc72739b5fe39ee
Body337 <xxxlarge6665@gmail.com>
2023-09-01 23:33:38 +0000

    fixed and modified

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,7 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "main.h"
-void rec_num(unsigned long int n);
 /**
   * print_binary - Prints the binary representation of a number
   * @n: number
@@ -10,28 +9,9 @@ void rec_num(unsigned long int n);
   */
 void print_binary(unsigned long int n)
 {
-	if (n == 0)
+	if (n > 1)
 	{
-		_putchar('0');
-		return;
+		print_binary(n >> 1);
 	}
-	rec_num(n);
-}
-/**
-  * rec_num - print the binary in rec
-  * @n: ...
-  *
-  * Return: ...
-  */
-void rec_num(unsigned long int n)
-{
-	if (n < 1)
-		return;
-
-	_divide(n >> 1);
-
-	if (n & 1)
-		_putchar('1');
-	else
-		_putchar('0');
+	_putchar((n & 1) ? '1' : '0');
 }
