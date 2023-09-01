ad0e63e3209657a1ee77b01888c1e479466e5806
Body337 <xxxlarge6665@gmail.com>
2023-09-01 23:51:33 +0000

    fixed?

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -15,9 +15,5 @@ int get_bit(unsigned long int n, unsigned int index)
 		return (-1);
 	n = n >> index;
 
-	if (n)
-	{
-		return ((n & 1) ? 1 : 0);
-	}
-	return (-1);
+	return ((n & 1) ? 1 : 0);}
 }
