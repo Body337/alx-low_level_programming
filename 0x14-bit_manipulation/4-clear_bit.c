9f95da455f7fa9418067c8c2d89c0db6a594cfe1
Body337 <xxxlarge6665@gmail.com>
2023-09-02 00:14:46 +0000

    fixed?

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -13,5 +13,7 @@ int clear_bit(unsigned long int *n, unsigned int index)
 {
 	if (index > sizeof(unsigned long int) * 8)
 		return (-1);
-	return ((*n &= ~(1 << index)) ? 1 : -1);
+	*n &= (~(1 << index));
+
+	return (1);
 }
