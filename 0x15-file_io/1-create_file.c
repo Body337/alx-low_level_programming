833c722ae6250110d0ed947664e73e6867b56956
Body337 <xxxlarge6665@gmail.com>
2023-09-05 01:14:28 +0000

    created?

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,36 @@
+#include "main.h"
+/**
+ * create_file - creates a file
+ * @filename: the file to be created
+ * @text_content: a null terminated string
+ *
+ * Return: 1 on success -1 of faill
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+
+	fd = open(filename, O_RDWR | O_CREATE | O_TRUNC, S_IRUSR | O_IWUSR);
+
+	if (fd == -1)
+		return (-1);
+
+	if (text_content != NULL)
+		write(fd, text_content, _strlen(text_content));
+	return (0);
+}
+/**
+  * _strlen - Returns the length of a string
+  * @s: String to count
+  *
+  * Return: String length
+  */
+int _strlen(char *s)
+{
+	int c = 0;
+
+	while (s[c])
+		c++;
+
+	return (c);
+}
